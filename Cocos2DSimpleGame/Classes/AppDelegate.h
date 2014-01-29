82e1e360d244abd506b037513e06862d48caabe0
Michel Mansour <michel.mansour@gmail.com>
2014-01-28 20:33:54 -0500

    Initial Commit

---
+++ Cocos2DSimpleGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Cocos2DSimpleGame
+//
+//  Created by Michel Mansour on 1/28/14.
+//  Copyright Michel Mansour 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Cocos2DSimpleGame/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2DSimpleGame
+//
+//  Created by Michel Mansour on 1/28/14.
+//  Copyright Michel Mansour 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
